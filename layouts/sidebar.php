bc9e2c82bf7cccf28ba2f4abbbe09bb88a2cd58b
speedjin1995 <speedjin1995@gmail.com>
2026-08-24 23:55:27 +0800

    change width

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -8,7 +8,7 @@
                 <img src="assets/images/logo-sm.png" alt="" height="70">
             </span>
             <span class="logo-lg">
-                <img src="assets/images/logo.png" alt="" height="60">
+                <img src="assets/images/logo.png" alt="" width="100%">
             </span>
         </a>
         <!-- Light Logo-->
@@ -17,7 +17,7 @@
                 <img src="assets/images/logo-sm.png" alt="" height="70">
             </span>
             <span class="logo-lg">
-                <img src="assets/images/logo.png" alt="" height="60">
+                <img src="assets/images/logo.png" alt="" width="100%">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
